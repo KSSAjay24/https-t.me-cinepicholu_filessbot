c887936c192f9c36b630199f54ae60e576512779
kssajay <149785052+KSSAjay24@users.noreply.github.com>
2023-12-19 10:11:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/vipuljainit18/Link-Shortner-Bot 
+  git clonehttps://github.com/KSSAjay24/https-t.me-cinepicholu_filessbot/tree/main
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
